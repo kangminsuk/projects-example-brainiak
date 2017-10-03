b66bd792288aea38bb0f81f93b2045f70229f0cd
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-10-03 08:51:10 -0700

    add an initial requirements.

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+pip3 install numpy==1.11.0
+pip3 install pandas==0.19.1
+pip3 install scipy==0.18.1
+pip3 install seaborn==0.7.1
+
+DATADIR=/usr/local/share/datasets
+mkdir -p $DATADIR
+# wget -O $DATADIR/names.csv.gz https://s3.amazonaws.com/assets.datacamp.com/production/project_2/datasets/names.csv.gz
+# wget -O $DATADIR/lifetables.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_2/datasets/lifetables.csv
+
