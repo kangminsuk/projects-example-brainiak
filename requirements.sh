0a88fdc037ca005d011fc6d4019f70b0f8a13dd5
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-10-04 16:16:54 -0700

    update dataset links.

--- requirements.sh
+++ requirements.sh
@@ -5,5 +5,5 @@ pip3 install seaborn==0.7.1
 
 DATADIR=/usr/local/share/datasets
 mkdir -p $DATADIR
-wget -O $DATADIR/colors.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_9/datasets/colors.csv
-wget -O $DATADIR/sets.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_9/datasets/sets.csv
+wget -O $DATADIR/colors.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/colors.csv
+wget -O $DATADIR/sets.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/sets.csv
