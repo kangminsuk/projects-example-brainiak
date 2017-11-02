c8375fb39794bd123344c097a59714ab68a08343
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2017-11-02 12:14:58 -0700

    update DATADIR to /home/repl/datasets.

--- requirements.sh
+++ requirements.sh
@@ -3,7 +3,7 @@ pip3 install pandas==0.19.1
 pip3 install scipy==0.18.1
 pip3 install seaborn==0.7.1
 
-DATADIR=/usr/local/share/datasets
+DATADIR=/home/repl/datasets
 mkdir -p $DATADIR
 wget -O $DATADIR/colors.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/colors.csv
 wget -O $DATADIR/sets.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/sets.csv
