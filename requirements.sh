b0d7edb94be89783f7de9de3479285636dfb1103
Michael Chow <machow@princeton.edu>
2017-11-13 07:54:43 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,14 +1,1 @@
-pip3 install numpy==1.11.0
-pip3 install pandas==0.19.1
-pip3 install scipy==0.18.1
-pip3 install seaborn==0.7.1
-
-DATADIR=/usr/local/share/datasets
-mkdir -p $DATADIR
-wget -O $DATADIR/colors.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/colors.csv
-wget -O $DATADIR/sets.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/sets.csv
-
-DATADIRH=/home/repl/datasets
-mkdir -p $DATADIRH
-wget -O $DATADIRH/colors.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/colors.csv
-wget -O $DATADIRH/sets.csv https://s3.amazonaws.com/assets.datacamp.com/production/project_10/datasets/sets.csv
+pip3 install protowhat protobackend asttokens
