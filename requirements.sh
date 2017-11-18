19ccd38b83ef7846ea06d82ab2ca16f27b14e967
Michael Chow <machow@princeton.edu>
2017-11-17 22:56:44 -0500

    initial commit of Manoj's example notebook

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,10 @@
-pip3 install protowhat protobackend asttokens
+# Python dependencies -----------------
+pip3 install sklearn
+
+# Data --------------------------------
+DATADIR=/home/repl/datasets
+DATA_URL=https://s3.amazonaws.com/assets.datacamp.com/projects/machow-pni-demo/BE_roi_data.mat
+
+mkdir -p $DATADIR
+
+wget -O $DATADIR/BE_roi_data.mat $DATA_URL
